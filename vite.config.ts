a336dd78ace967626239bc1f8b4c793c856c1d6b
ckdgus1010 <ckdgus3735@gmail.com>
2025-08-08 23:51:02 +0900

    fix: vite.config.js 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ckdgus1010.github.io/crypto-tracker-clone"
+  base: "/crypto-tracker-clone/"
 })
