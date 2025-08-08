5bef05c7646d0871cefc5c635c379ccafc9530f5
ckdgus1010 <ckdgus3735@gmail.com>
2025-08-08 23:36:49 +0900

    build: 빌드 파일 생성

--- src/apis/CoinDetailsApi.ts
+++ src/apis/CoinDetailsApi.ts
@@ -1,19 +1,19 @@
 const BASE_URL = "https://api.coinpaprika.com/v1";
 const OHLCV_URL = "https://ohlcv-api.nomadcoders.workers.dev/";
 
-export async function fetchCoinInformation(coinId: string) {
+export async function fetchCoinInformation(coinId: string | undefined) {
     return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
         response.json()
     );
 }
 
-export async function fetchCoinTickers(coinId: string) {
+export async function fetchCoinTickers(coinId: string | undefined) {
     return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
         response.json()
     );
 }
 
-export async function fetchCoinHistory(coinId: string) {
+export async function fetchCoinHistory(coinId: string | undefined) {
     return fetch(`${OHLCV_URL}?coinId=${coinId}`).then((response) =>
         response.json()
     );
